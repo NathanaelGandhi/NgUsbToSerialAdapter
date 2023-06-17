5a34ce986aaf1daa942b6cb947efa5c9ceba97fa
Nathanael Gandhi <nat11public@gmail.com>
2023-06-18 01:18:07 +1000

    feat(app): main c file boilerplate

---
+++ app/src/main.c
@@ -0,0 +1,16 @@
+/*##############################################################################
+# Title:    NG USB to Serial Adapter
+# Author:   Nathanael Gandhi
+# Notes:    https://github.com/NathanaelGandhi/NgUsbToSerialAdapter
+##############################################################################*/
+
+static void main_setup(void) {}
+
+int main(void) {
+    main_setup();
+
+    // main loop
+    while (1) {
+    }
+    return 0;
+}
